f532baf3740419bd22a399e904efa1ffc2ca350a
Dominik Wilk <dominik.wilk@arpinformatyka.pl>
2023-08-28 07:00:25 +0200

    props with singular option

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface ButtonProps {
-    children: React.ReactNode[]
+    children: React.ReactNode[] | React.ReactNode;
 }
 
 const Button = ({children}: ButtonProps): React.JSX.Element => {
